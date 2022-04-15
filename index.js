72eebe119a67ef7326cadcfabd34bc287d83717c
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-16 00:26:47 +0700

    challenge 1 solution 2

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 //////// CHALLENGE 1
 // factoralize a number
 /// solution 1
+// function factorialize(num) {
+// 	let result = 1;
+// 	for (let i = num; i > 0; i--) {
+// 		result *= i;
+// 	}
+// 	return result;
+// }
+// console.log(factorialize(5));
+// console.log(factorialize(10));
+// console.log(factorialize(20));
+// console.log(factorialize(0));
+//////////////////
+/// solution 2
 function factorialize(num) {
-	let result = 1;
-	for (let i = num; i > 0; i--) {
-		result *= i;
+	if (num === 0) {
+		return 1;
 	}
-	return result;
+	return num * factorialize(num - 1);
 }
 console.log(factorialize(5));
 console.log(factorialize(10));
