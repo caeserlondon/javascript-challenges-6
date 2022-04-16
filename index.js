d808a3e17d3ed23f02096aa9bf655c6cf8151efd
Caeser F Ibrahim (he/him) <caeserlondon@gmail.com>
2022-04-16 16:07:01 +0700

    challenge 2 solution 2

--- index.js
+++ index.js
@@ -42,13 +42,31 @@
 ////////////////////////////////////////
 
 /// chalenge 2 : capitalize the first letters
+/// /// solution 1
+// function capitalize(str) {
+// 	let tempArr = str.toLowerCase().split(" ");
+// 	let result = tempArr.map((val) =>
+// 		val.replace(val.charAt(0), val.charAt(0).toUpperCase())
+// 	);
+// 	return result.join(" ");
+// }
+// console.log(capitalize("I'm a little tea pot"));
+// console.log(capitalize("caeser ibrahim"));
 
+//////////////////
+//// /// solution 2
 function capitalize(str) {
-	let tempArr = str.toLowerCase().split(" ");
-	let result = tempArr.map((val) =>
-		val.replace(val.charAt(0), val.charAt(0).toUpperCase())
-	);
-	return result.join(" ");
+	str = str.split(" ");
+
+	for (let i = 0; i < str.length; i++) {
+		str[i] = str[i].toLowerCase().split("");
+
+		str[i][0] = str[i][0].toUpperCase();
+
+		str[i] = str[i].join("");
+	}
+	return str.join(" ");
 }
+
 console.log(capitalize("I'm a little tea pot"));
 console.log(capitalize("caeser ibrahim"));
